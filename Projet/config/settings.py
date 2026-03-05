9e6fda47bee1cb5a8530827c6c7f8b707908cfa8
Chibane Sarah <sarah.chibane@etu.univ-orleans.fr>
2026-03-05 20:26:31 +0100

    Fix Render deployment

--- Projet/config/settings.py
+++ Projet/config/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-# Import dj-database-url at the beginning of the file
-import dj_database_url
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
